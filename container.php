8c7f02a243904e7da0fc0f9a2c593bfb444d1c00
Ismael <ismaelberfer@gmail.com>
2015-12-03 23:34:21 +0100

    first commit

---
+++ container.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Styde;
+
+use Closure;
+use InvalidArgumentException;
+use ReflectionClass;
+
+class Container
+{
+    protected $shared = [];
+    protected $bindings = [];
+
+    public function bind($name, $resolver)
+    {
+        $this->bindings[$name] = [
+            'resolver' => $resolver
+        ];
+    }
+
+    public function instance($name, $object)
+    {
+        $this->shared[$name] = $object;
+    }
+
+    public function make($name)
+    {
+        if (isset ($this->shared[$name])) {
+            return $this->shared[$name];
+        }
+
+        $resolver = $this->bindings[$name]['resolver'];
+
+        if ($resolver instanceof Closure) {
+            return $resolver($this);
+        }
+
+        return $this->build($resolver);
+    }
+
+    protected function build($class)
+    {
+        $reflection = new ReflectionClass($class);
+
+        if(! $reflection->isInstantiable()) {
+            throw new InvalidArgumentException("$class is not instantiable");
+        }
+
+        if(is_null($constructor = $reflection->getConstructor())) {
+            return new $class;
+        }
+
+        $dependency = $this->instanceArgs($constructor->getParameters()); 
+
+        return $reflection->newInstanceArgs($dependency);
+    }
+
+    protected function instanceArgs($parameters) 
+    {
+        return $dependency = array_map(function ($parameter) {
+            
+            return $this->build($parameter->getClass()->getName());
+        }, $parameters);
+    }
+}
